b9a4898d29031394ee2551c2bd560c80dcf06cd3
Leeyu <38371056+leeyu0911@users.noreply.github.com>
2020-10-24 16:30:46 +0800

    Update main.py

--- Homework_1/main.py
+++ Homework_1/main.py
@@ -1,22 +1,25 @@
+"""
+macOS: 10.15.7（19H2）
+python: 3.7
+pycharm: 2020.2.3
+"""
+from Homework_1.reference.pyfpgrowth import *
+
+from Homework_1.FP_Growth import *
 from Homework_1.apriori_algorithm import *
 
 
-# test from lecture
-def test_from_lecture(chose_type=1):
+def deal_lecture_data(chose_type=1):
     """ result:
     str:
-    [0,
-    {'a': 2, 'c': 3, 'b': 3, 'e': 3},
-    {('a', 'c'): 2, ('b', 'c'): 2, ('b', 'e'): 3, ('c', 'e'): 2},
-    {('b', 'c', 'e'): 2},
-    {}]
+    {'a': 2, 'c': 3, 'b': 3, 'e': 3,
+     ('a', 'c'): 2, ('b', 'c'): 2, ('b', 'e'): 3, ('c', 'e'): 2,
+     ('b', 'c', 'e'): 2}
 
     int:
-    [0,
-    {'11': 2, '33': 3, '22': 3, '55': 3},
-    {('11', '33'): 2, ('22', '33'): 2, ('22', '55'): 3, ('33', '55'): 2},
-    {('22', '33', '55'): 2},
-    {}]
+    {'11': 2, '33': 3, '22': 3, '55': 3,
+     ('11', '33'): 2, ('22', '33'): 2, ('22', '55'): 3, ('33', '55'): 2,
+     ('22', '33', '55'): 2}
     """
     if chose_type == 1:
         database = [['a', 'c', 'd'],
@@ -28,14 +31,18 @@ def test_from_lecture(chose_type=1):
                     [22, 33, 55],
                     [11, 22, 33, 55],
                     [22, 55]]
-
-    L = apriori(database, 2)
-    print('test from lecture:', L)
+    return database
 
 
-# test from kaggle (groceries_datatest)
-def test_from_kaggle(min_support=50):
-    filename = './dataset/groceries - groceries.csv'
+def deal_kaggle_data(filename='./dataset/groceries - groceries.csv'):
+    """
+    https://www.kaggle.com/irfanasrullah/groceries?select=groceries+-+groceries.csv
+    format:
+    Item(s),Item 1,Item 2,Item 3,Item 4,Item 5,Item 6,Item 7,Item 8,Item 9,Item 10,Item 11,Item 12,Item 13,Item 14,Item 15,Item 16,Item 17,Item 18,Item 19,Item 20,Item 21,Item 22,Item 23,Item 24,Item 25,Item 26,Item 27,Item 28,Item 29,Item 30,Item 31,Item 32
+    4,citrus fruit,semi-finished bread,margarine,ready soups
+    3,tropical fruit,yogurt,coffee
+    ...
+    """
     with open(filename) as f:
         content = f.readlines()
     content = [s.strip() for s in content]
@@ -45,14 +52,10 @@ def test_from_kaggle(min_support=50):
     for c in content[1:]:
         temp = c.split(',')[1:]
         transactions.append(temp)
-    # print(transactions)
-
-    L_test_data_csv = apriori(transactions, min_support)
-    print('L_test_data_csv:', L_test_data_csv)
+    return transactions
 
 
-# test from IBM data
-def test_from_IBMdata(filename='./dataset/test_data.csv', min_support=18):
+def deal_IBM_data(filename='./dataset/test_data.csv'):
     """
     format:
     TID,item
@@ -70,10 +73,74 @@ def test_from_IBMdata(filename='./dataset/test_data.csv', min_support=18):
         temp = c.replace(',', ' ').split(' ')[1:]
         transactions.append(temp)
     # print(transactions)
+    return transactions
 
-    L_test_data_csv = apriori(transactions, min_support)
-    print('L_test_data_csv:', L_test_data_csv)
+
+class Apriori_test_set:
+
+    # test from lecture
+    @staticmethod
+    def test_from_lecture(chose_type=1, min_support=2):
+        transactions = deal_lecture_data(chose_type)
+        return apriori(transactions, min_support)
+
+    # test from kaggle (groceries_datatest)
+    @staticmethod
+    def test_from_kaggle(min_support=50):
+        transactions = deal_kaggle_data()
+        return apriori(transactions, min_support)
+        # print('L_test_data_csv:', L_test_data_csv)
+
+    # test from IBM data
+    @staticmethod
+    def test_from_IBMdata(min_support=18):
+        transactions = deal_IBM_data()
+        return apriori(transactions, min_support)
+        # print('L_test_data_csv:', L_test_data_csv)
+
+
+class FP_Growth_test_set:
+
+    @staticmethod
+    def test_from_lecture(chose_type=1, min_support=2):
+        transactions = deal_lecture_data(chose_type)
+        FP_Tree(transactions, min_support).result()
+
+    @staticmethod
+    def test_from_kaggle(min_support=50):
+        transactions = deal_kaggle_data()
+
+    @staticmethod
+    def test_from_IBMdata(min_support=18):
+        transactions = deal_IBM_data()
+
+
+def to_file(content: dict, filename):
+    # content = list(content.items())
+    f = open('./result/' + filename, 'w+')
+    # for i in content:
+    #     # print(i)
+    #     f.write(str(i) + '\n')
+    f.write(str(content))
+    f.close()
+
 
 
 if __name__ == '__main__':
-    pass
+    # Apriori test
+    task = [Apriori_test_set.test_from_lecture, Apriori_test_set.test_from_kaggle,
+            Apriori_test_set.test_from_IBMdata]
+    arg = [2, 600, 50]
+    print(str(task[0]).split(' '))  # ['<function', 'Apriori_test_set.test_from_lecture', 'at', '0x7fa8f2d62d40>']
+    for i, t in enumerate(task):
+        to_file(t(arg[i]), str(task[i]).split(' ')[1] + '.txt')
+
+
+    # FP_Growth test
+
+    # FP_Growth_test_set.test_from_lecture()
+
+    # print((find_frequent_patterns(transactions, 18)))
+    # print((find_frequent_patterns(deal_lecture_data(), 2)))
+
+    # print(deal_IBM_data())
